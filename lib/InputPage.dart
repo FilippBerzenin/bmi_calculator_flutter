0c345ede9f763e44a6d86c286d36f7ab16183267
Fylyp Berzenin <FBerzenin@Amerilife.com>
2022-02-07 19:16:26 +0200

    added slider coloring

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -97,17 +97,26 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                    value: (hight.toDouble()),
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (newValue) {
-                      setState(() {
-                        hight = newValue.round();
-                      });
-                    },
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                    ),
+                    child: Slider(
+                      value: (hight.toDouble()),
+                      min: 120.0,
+                      max: 220.0,
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (newValue) {
+                        setState(() {
+                          hight = newValue.round();
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
